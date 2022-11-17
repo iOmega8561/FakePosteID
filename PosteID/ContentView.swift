a74138323619eb28418aacba42b93dd243167234
Giuseppe <giusepperocco38@gmail.com>
2022-11-17 17:47:12 +0100

    Initiated work on login page

--- PosteID/ContentView.swift
+++ PosteID/ContentView.swift
@@ -7,15 +7,82 @@
 
 import SwiftUI
 
+struct Landscape: View {
+    @State var PosteIDCode: Int = 0
+    
+    var body: some View {
+        NavigationStack() {
+            ZStack {
+                
+                Circle()
+                    .scaledToFit()
+                    .frame(maxWidth: 600.0)
+                    .foregroundColor(Color("PosteYellow"))
+                    .opacity(0.6)
+                    .offset(x: -280.0, y: 400.0)
+                
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        Spacer()
+                        
+                        Rectangle()
+                            .cornerRadius(5.0)
+                            .foregroundColor(Color("PosteYellow"))
+                            .frame(maxWidth: 300.0, maxHeight: 100.0)
+                            .overlay() {
+                                VStack {
+                                    Image("PosteIDLogo")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(maxWidth: 280.0)
+                                        .border(Color("PosteBlue"))
+                                    
+                                    
+                                }
+                            }
+                        
+                        Text("L'identità digitale di Poste Italiane")
+                            .font(.title)
+                            .frame(maxWidth: 500.0)
+                        
+                        Spacer()
+                        Spacer()
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Spacer()
+                        
+                        Text("Bentornato Giuseppe!\nInserisci il tuo codice PosteID")
+                            .font(.title)
+                            .multilineTextAlignment(.center)
+                            .frame(maxWidth: 500.0)
+                        
+                        Spacer()
+                        
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                }
+            }
+        }.ignoresSafeArea()
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+             if geometry.size.height > geometry.size.width {
+                    Text("Portrait")
+             } else {
+                    Landscape()
+             }
         }
-        .padding()
     }
 }
 
---
+++ Views/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  PosteID
+//
+//  Created by Giuseppe Rocco on 17/11/22.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
