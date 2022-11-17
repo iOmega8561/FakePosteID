114dedb8c229e8ddbae4aefa94ba72411edc8602
Giuseppe <giusepperocco38@gmail.com>
2022-11-17 16:08:50 +0100

    Initial Commit

---
+++ PosteID/PosteIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosteIDApp.swift
+//  PosteID
+//
+//  Created by Giuseppe Rocco on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PosteIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
