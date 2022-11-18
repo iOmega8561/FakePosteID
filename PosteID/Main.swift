f063a1cfe5da7f1fd47e680b33eac4d5f935c722
Giuseppe <giusepperocco38@gmail.com>
2022-11-18 11:37:01 +0100

    Finished Login Screen, starting Main View

--- PosteID/Main.swift
+++ PosteID/Main.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct Login: View {
+struct Main: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Main view")
     }
 }
 
-struct Login_Previews: PreviewProvider {
+struct Main_Previews: PreviewProvider {
     static var previews: some View {
-        Login()
+        Main()
     }
 }
--- PosteID/PosteIDApp.swift
+++ PosteID/PosteIDApp.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.all //By default you want all your views to rotate freely
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask{
+        return AppDelegate.orientationLock
+    }
+}
+
 @main
 struct PosteIDApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Login()
         }
     }
 }
