a2baf2012216a0f383f72fe1f8ed9a44297811dc
Giuseppe <giusepperocco38@gmail.com>
2022-11-23 15:19:46 +0100

    LoginForm is not inside a rectangle anymore

--- Login/LoginForm.swift
+++ Login/LoginForm.swift
@@ -1,8 +1,8 @@
 //
-//  Form.swift
+//  LoginForm2.swift
 //  PosteID
 //
-//  Created by Giuseppe Rocco on 21/11/22.
+//  Created by Giuseppe Rocco on 23/11/22.
 //
 
 import SwiftUI
@@ -19,70 +19,54 @@ struct LoginForm: View {
         GeometryReader { proxy in
             NavigationStack {
                 ZStack {
-                    
-                    if colorScheme == .dark {
-                        Color(red: 0.0, green: 0.0, blue: 0.0).ignoresSafeArea()
-                        
-                    } else {
+                    if colorScheme != .dark {
                         Color(red: 0.949, green: 0.949, blue: 0.971).ignoresSafeArea()
-                        
+                            
                     }
                     
-                    Rectangle()
-                        .fill(colorScheme == .dark ? Color(red: 0.0, green: 0.0, blue: 0.0):Color(red: 0.949, green: 0.949, blue: 0.971))
-                        .cornerRadius(30.0)
-                        .shadow(color: UIDevice.current.localizedModel == "iPhone" ? .black:Color("PosteYellow"), radius: UIDevice.current.localizedModel == "iPhone" ? 0.0:6.0)
-                        .overlay() {
-                            Rectangle()
-                                .fill(colorScheme == .dark ? Color(red: 0.0, green: 0.0, blue: 0.0):Color(red: 0.949, green: 0.949, blue: 0.971))
-                                .overlay() {
-                                    VStack {
-                                        Spacer()
-                                        
-                                        Rectangle()
-                                            .cornerRadius(10.0)
-                                            .foregroundColor(Color("PosteYellow"))
-                                            .frame(maxWidth: 300.0, maxHeight: 100.0)
-                                            .shadow(color: Color("PosteYellow"), radius: 6.0)
-                                            .overlay() {
-                                                Image("PosteIDLogo")
-                                                    .resizable()
-                                                    .scaledToFit()
-                                                    .frame(maxWidth: 280.0)
-                                            }
-                                        
-                                        Spacer()
-                                        
-                                        Form {
-                                            Section(header: Text("Username")) {
-                                                TextField("Your PosteID Username", text: $username)
-                                            }
-                                            
-                                            Section(header: Text("Password")) {
-                                                SecureField("Your PosteID Password", text: $password)
-                                            }.onSubmit {
-                                                isUnlocked.toggle()
-                                            }
-                                        }
-                                        .frame(maxWidth: 400.0, maxHeight: proxy.size.height * (UIDevice.current.localizedModel == "iPhone" ? 0.25:(proxy.size.width > proxy.size.height ? 0.25:0.17)))
-                                        
-                                        Spacer()
-                                        
-                                        if !isUnlocked {
-                                            BeforeUnlock(isUnlocked: $isUnlocked)
-                                            
-                                        } else {
-                                            AfterUnlock()
-                                        }
-                                        
-                                        Spacer()
-                                    }
-                                }
-                                .cornerRadius(25.0)
-                                .frame(maxWidth: proxy.size.width*(UIDevice.current.localizedModel == "iPhone" ? 0.97:0.58), maxHeight: proxy.size.height*(UIDevice.current.localizedModel == "iPhone" ? 0.92:0.7))
+                    VStack {
+                        Spacer()
+                        
+                        Rectangle()
+                            .cornerRadius(10.0)
+                            .foregroundColor(Color("PosteYellow"))
+                            .frame(maxWidth: 340.0, maxHeight: 110.0)
+                            .shadow(color: Color("PosteYellow"), radius: 6.0)
+                            .overlay() {
+                                Image("PosteIDLogo")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(maxWidth: 320.0)
+                            }
+                        
+                        Spacer()
+                        
+                        Form {
+                            Section(header: Text("Username")) {
+                                TextField("Your PosteID Username", text: $username)
+                            }
+                            
+                            Section(header: Text("Password")) {
+                                SecureField("Your PosteID Password", text: $password)
+                            }.onSubmit {
+                                isUnlocked.toggle()
+                            }
                         }
-                        .frame(maxWidth: proxy.size.width*(UIDevice.current.localizedModel == "iPhone" ? 1.0:(proxy.size.width > proxy.size.height ? 0.59:0.60)), maxHeight: proxy.size.height*(UIDevice.current.localizedModel == "iPhone" ? 0.95:0.715))
-                    
+                        .frame(maxWidth: 400.0, maxHeight: 200.0)
+                        
+                        Spacer()
+                        
+                        if UIDevice.current.localizedModel != "iPhone" && proxy.size.height > 400.0 {
+                            
+                            if !isUnlocked {
+                                BeforeUnlock(isUnlocked: $isUnlocked)
+                                
+                            } else {
+                                AfterUnlock()
+                            }
+                        }
+                        Spacer()
+                    }
                 }
             }
         }
@@ -99,7 +83,6 @@ struct LoginForm: View {
         }
     }
 }
-
 
 struct LoginForm_Previews: PreviewProvider {
     static var previews: some View {
